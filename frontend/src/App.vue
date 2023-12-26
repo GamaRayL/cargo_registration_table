1b369134d24475ff136ed41c1e9df09e6cf44480
Gama_Ray <gamaizingg@gmail.com>
2023-12-26 14:47:48 +0300

    Установка Vite + Vue 3

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--  <navbar></navbar>-->
+  <div class="app"></div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
